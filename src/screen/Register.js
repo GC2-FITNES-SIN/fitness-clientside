d58daad455e930dc896bfdbd463061a183fb9320
fahrizaichsani <fahrizaichsanirafif@gmail.com>
2024-02-18 14:41:58 +0700

    feat: Register page

--- src/screen/Register.js
+++ src/screen/Register.js
@@ -1,10 +1,54 @@
 import React from "react";
-import { Container, TextCustom } from "./Styled";
+import { Button, Container, ContainerFlexSameFlex, TextCustom, TextInput } from "./Styled";
 
 const Register = () => {
 	return (
-		<Container>
-			<TextCustom>Register</TextCustom>
+		<Container $padding={"20px"}>
+			<ContainerFlexSameFlex $column $justifyContent={"flex-end"}>
+				<TextCustom $fontSize={"20px"} $color={"#1D1617"}>
+					Hey there,
+				</TextCustom>
+				<TextCustom $fontWeight={"700"} $fontSize={"20px"} $color={"#1D1617"}>
+					Create an Account
+				</TextCustom>
+			</ContainerFlexSameFlex>
+			<ContainerFlexSameFlex $column $gap={"15px"} $justifyContent={"center"} $padding={"30px 10px 10px 10px"}>
+				<TextInput $backgroundColor={"#F7F8F8"} $borderRadius={"14px"} $height={"48px"} placeholder="First Name" $fontSize={"12px"}>
+
+				</TextInput>
+				<TextInput $backgroundColor={"#F7F8F8"} $borderRadius={"14px"} $height={"48px"} placeholder="Last Name" $fontSize={"12px"}>
+
+				</TextInput>
+				<TextInput $backgroundColor={"#F7F8F8"} $borderRadius={"14px"} $height={"48px"} placeholder="Email" $fontSize={"12px"}>
+
+				</TextInput>
+				<TextInput $backgroundColor={"#F7F8F8"} $borderRadius={"14px"} $height={"48px"} placeholder="Password" $fontSize={"12px"}>
+
+				</TextInput>
+			</ContainerFlexSameFlex>
+			<ContainerFlexSameFlex $column $gap={"10px"} $justifyContent={"center"}>
+				<Button $backgroundColor={"grey"} $height={"60px"} $borderRadius={"99px"} $padding={"20px 0px 0px 0px"}
+					style={{
+						shadowColor: "#000",
+						shadowOffset: {
+							width: 0,
+							height: 10,
+						},
+						shadowOpacity: 0.3,
+						shadowRadius: 22,
+						elevation: 3,
+						marginTop: 5,
+						marginBottom: 5,
+					}}
+				>
+					<TextCustom $fontWeight={"700"} $fontSize={"16px"} $color={"#FFFFFF"}>
+						Register
+					</TextCustom>
+				</Button>
+				<TextCustom $fontSize={"14px"} $color={"#1D1617"}>
+					Already have an account? Login
+				</TextCustom>
+			</ContainerFlexSameFlex>
 		</Container>
 	);
 };
