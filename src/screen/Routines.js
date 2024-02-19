94140ed9fdd0fdda948207f14787106e30edbc5e
fikilifadly <fikilifadly@gmail.com>
2024-02-19 22:58:50 +0700

    feat: add routines screen

--- src/screen/Routines.js
+++ src/screen/Routines.js
@@ -1,14 +1,152 @@
 import React from "react";
-import { Container, ContainerFlexSameFlex } from "./Styled";
+import { Container, ContainerFlexSameFlex, ScrollView, TextCustom } from "./Styled";
+import { Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
 const Routines = () => {
 	return (
-		<Container>
-			<ContainerFlexSameFlex $column $backgroundColor={"#1b1b1d"}>
-				{/* <CarouselCustom /> */}
+		<SafeAreaView style={{ flex: 1 }}>
+			<StatusBar backgroundColor="#1b1b1d" barStyle="light-content" />
+			<ScrollView>
+				<Container>
+					<ContainerFlexSameFlex $column $backgroundColor={"#1b1b1d"} $padding={"15px"} $gap={"15px"}>
+						<TextCustom $textAlign={"left"} $color={"#bd54eb"}>
+							Workout Routines
+						</TextCustom>
+						{/* loop here */}
+						<ContainerFlexSameFlex $column $gap={"5px"}>
+							<TextCustom $fontSize={"24px"} $color={"white"} $fontWeight={"700"} $textAlign={"left"}>
+								Obesity
+							</TextCustom>
+							<ContainerFlexSameFlex $width={"100%"} $borderRadius={"16px"} $flex={"1"}>
+								<ContainerFlexSameFlex $flex={"1"} $width={"150px"} $height={"150px"} style={{ position: "relative", overflow: "hidden" }}>
+									<Image
+										source={{
+											uri: "https://images.unsplash.com/photo-1521804906057-1df8fdb718b7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Ym9keSUyMG1hc3MlMjBpbmRleCUyMGZpdG5lc3N8ZW58MHx8MHx8fDA%3D",
+										}}
+										style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: 16, zIndex: 10 }}
+									/>
+								</ContainerFlexSameFlex>
+								<ContainerFlexSameFlex $flex={"1"} $width={"150px"} $height={"150px"} style={{ position: "relative" }}>
+									<Image
+										source={{
+											uri: "https://images.unsplash.com/photo-1521804906057-1df8fdb718b7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Ym9keSUyMG1hc3MlMjBpbmRleCUyMGZpdG5lc3N8ZW58MHx8MHx8fDA%3D",
+										}}
+										style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: 16, zIndex: 10 }}
+									/>
+								</ContainerFlexSameFlex>
+								<ContainerFlexSameFlex $flex={"1"} $width={"150px"} $height={"150px"} style={{ position: "relative" }}>
+									<Image
+										source={{
+											uri: "https://images.unsplash.com/photo-1521804906057-1df8fdb718b7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Ym9keSUyMG1hc3MlMjBpbmRleCUyMGZpdG5lc3N8ZW58MHx8MHx8fDA%3D",
+										}}
+										style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: 16, zIndex: 10 }}
+									/>
+								</ContainerFlexSameFlex>
+							</ContainerFlexSameFlex>
+						</ContainerFlexSameFlex>
+						{/* end loop*/}
 
-			</ContainerFlexSameFlex>
-		</Container>
+						<ContainerFlexSameFlex $column>
+							<TextCustom $fontSize={"24px"} $color={"white"} $fontWeight={"700"} $textAlign={"left"}>
+								OverWeight
+							</TextCustom>
+							<ContainerFlexSameFlex $width={"100%"} $borderRadius={"16px"} $flex={"1"}>
+								<ContainerFlexSameFlex $flex={"1"} $width={"150px"} $height={"150px"} style={{ position: "relative" }}>
+									<Image
+										source={{
+											uri: "https://images.unsplash.com/photo-1521804906057-1df8fdb718b7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Ym9keSUyMG1hc3MlMjBpbmRleCUyMGZpdG5lc3N8ZW58MHx8MHx8fDA%3D",
+										}}
+										style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: 16, zIndex: 10 }}
+									/>
+								</ContainerFlexSameFlex>
+								<ContainerFlexSameFlex $flex={"1"} $width={"150px"} $height={"150px"} style={{ position: "relative" }}>
+									<Image
+										source={{
+											uri: "https://images.unsplash.com/photo-1521804906057-1df8fdb718b7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Ym9keSUyMG1hc3MlMjBpbmRleCUyMGZpdG5lc3N8ZW58MHx8MHx8fDA%3D",
+										}}
+										style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: 16, zIndex: 10 }}
+									/>
+								</ContainerFlexSameFlex>
+								<ContainerFlexSameFlex $flex={"1"} $width={"150px"} $height={"150px"} style={{ position: "relative" }}>
+									<Image
+										source={{
+											uri: "https://images.unsplash.com/photo-1521804906057-1df8fdb718b7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Ym9keSUyMG1hc3MlMjBpbmRleCUyMGZpdG5lc3N8ZW58MHx8MHx8fDA%3D",
+										}}
+										style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: 16, zIndex: 10 }}
+									/>
+								</ContainerFlexSameFlex>
+							</ContainerFlexSameFlex>
+						</ContainerFlexSameFlex>
+
+						<ContainerFlexSameFlex $column>
+							<TextCustom $fontSize={"24px"} $color={"white"} $fontWeight={"700"} $textAlign={"left"}>
+								Normal
+							</TextCustom>
+							<ContainerFlexSameFlex $width={"100%"} $borderRadius={"16px"} $flex={"1"}>
+								<ContainerFlexSameFlex $flex={"1"} $width={"150px"} $height={"150px"} style={{ position: "relative" }}>
+									<Image
+										source={{
+											uri: "https://images.unsplash.com/photo-1521804906057-1df8fdb718b7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Ym9keSUyMG1hc3MlMjBpbmRleCUyMGZpdG5lc3N8ZW58MHx8MHx8fDA%3D",
+										}}
+										style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: 16, zIndex: 10 }}
+									/>
+								</ContainerFlexSameFlex>
+								<ContainerFlexSameFlex $flex={"1"} $width={"150px"} $height={"150px"} style={{ position: "relative" }}>
+									<Image
+										source={{
+											uri: "https://images.unsplash.com/photo-1521804906057-1df8fdb718b7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Ym9keSUyMG1hc3MlMjBpbmRleCUyMGZpdG5lc3N8ZW58MHx8MHx8fDA%3D",
+										}}
+										style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: 16, zIndex: 10 }}
+									/>
+								</ContainerFlexSameFlex>
+								<ContainerFlexSameFlex $flex={"1"} $width={"150px"} $height={"150px"} style={{ position: "relative" }}>
+									<Image
+										source={{
+											uri: "https://images.unsplash.com/photo-1521804906057-1df8fdb718b7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Ym9keSUyMG1hc3MlMjBpbmRleCUyMGZpdG5lc3N8ZW58MHx8MHx8fDA%3D",
+										}}
+										style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: 16, zIndex: 10 }}
+									/>
+								</ContainerFlexSameFlex>
+							</ContainerFlexSameFlex>
+						</ContainerFlexSameFlex>
+
+						<ContainerFlexSameFlex $column>
+							<TextCustom $fontSize={"24px"} $color={"white"} $fontWeight={"700"} $textAlign={"left"}>
+								Underweight
+							</TextCustom>
+							<ContainerFlexSameFlex $width={"100%"} $borderRadius={"16px"} $flex={"1"}>
+								<ContainerFlexSameFlex $flex={"1"} $width={"150px"} $height={"150px"} style={{ position: "relative" }}>
+									<Image
+										source={{
+											uri: "https://images.unsplash.com/photo-1521804906057-1df8fdb718b7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Ym9keSUyMG1hc3MlMjBpbmRleCUyMGZpdG5lc3N8ZW58MHx8MHx8fDA%3D",
+										}}
+										style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: 16, zIndex: 10 }}
+									/>
+								</ContainerFlexSameFlex>
+								<ContainerFlexSameFlex $flex={"1"} $width={"150px"} $height={"150px"} style={{ position: "relative" }}>
+									<Image
+										source={{
+											uri: "https://images.unsplash.com/photo-1521804906057-1df8fdb718b7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Ym9keSUyMG1hc3MlMjBpbmRleCUyMGZpdG5lc3N8ZW58MHx8MHx8fDA%3D",
+										}}
+										style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: 16, zIndex: 10 }}
+									/>
+								</ContainerFlexSameFlex>
+								<ContainerFlexSameFlex $flex={"1"} $width={"150px"} $height={"150px"} style={{ position: "relative" }}>
+									<Image
+										source={{
+											uri: "https://images.unsplash.com/photo-1521804906057-1df8fdb718b7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Ym9keSUyMG1hc3MlMjBpbmRleCUyMGZpdG5lc3N8ZW58MHx8MHx8fDA%3D",
+										}}
+										style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: 16, zIndex: 10 }}
+									/>
+								</ContainerFlexSameFlex>
+							</ContainerFlexSameFlex>
+						</ContainerFlexSameFlex>
+					</ContainerFlexSameFlex>
+				</Container>
+			</ScrollView>
+		</SafeAreaView>
 	);
 };
 
