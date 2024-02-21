ff5d7ea5bd21710eef1bfe65468d9b96190a75ee
fahrizaichsani <fahrizaichsanirafif@gmail.com>
2024-02-21 21:47:30 +0700

    feat: detail product fetching

--- src/screen/DetailRoutine.js
+++ src/screen/DetailRoutine.js
@@ -7,25 +7,24 @@ import * as SecureStore from 'expo-secure-store'
 import { useRoute } from '@react-navigation/native';
 
 
-const images = [
-	{
-		id: "1",
-		uri: "https://images.unsplash.com/flagged/photo-1556746834-cbb4a38ee593?q=80&w=3272&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-	},
-	{
-		id: "2",
-		uri: "https://images.unsplash.com/photo-1518365658347-c4906efc5636?q=80&w=3122&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-	},
-	{
-		id: "3",
-		uri: "https://images.unsplash.com/photo-1461896836934-ffe607ba8211?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-	},
-];
 const DetailRoutine = () => {
 	const route = useRoute()
-    const { id } = route.params
+	const { id } = route.params
 	const [detailRoutine, setDetailRoutine] = useState({});
 	const [currentIndex, setCurrentIndex] = useState(0);
+
+	const images = [
+		{
+			id: "1",
+			uri: detailRoutine.data.routineImageStart,
+			// uri: ""
+		},
+		{
+			id: "2",
+			uri: detailRoutine.data.routineImageEnd,
+			// uri: ""
+		}
+	];
 
 	const renderItem = ({ item }) => <Image source={{ uri: item.uri }} style={{ width: 363, height: 500 }} resizeMode="cover" />;
 
@@ -39,13 +38,12 @@ const DetailRoutine = () => {
 	const fetchDetailRoutine = async () => {
 		try {
 			const { data } = await axios({
-				url: `https://6cdd-2a09-bac1-3480-18-00-da-76.ngrok-free.app/routines/${id}`,
+				url: `https://9ebf-2a09-bac1-3480-18-00-da-76.ngrok-free.app/routines/${id}`,
 				method: "GET",
 				headers: {
 					Authorization: "Bearer " + SecureStore.getItem("access_token"),
 				},
 			});
-
 			setDetailRoutine(data);
 		} catch (error) {
 			console.log(error);
@@ -54,46 +52,44 @@ const DetailRoutine = () => {
 	useEffect(() => {
 		fetchDetailRoutine()
 	}, [])
+	console.log(detailRoutine);
 	return (
-		<SafeAreaView style={{ flex: 1 }}>
-			<Container $backgroundColor={"#1b1b1d"} $padding={"15px"} $gap={"15px"}>
-				<ContainerFlexSameFlex $flex={"2"} $padding={"0px"}>
-					<FlatList
-						data={images}
-						horizontal
-						pagingEnabled
-						showsHorizontalScrollIndicator={false}
-						keyExtractor={(item) => item.id}
-						renderItem={renderItem}
-						onViewableItemsChanged={onViewableItemsChanged}
-						viewabilityConfig={{ itemVisiblePercentThreshold: 50 }}
-					/>
-					<View
-						style={{
-							flexDirection: "row",
-							justifyContent: "center",
-							alignItems: "center",
-							marginTop: 10,
-						}}
-					></View>
+
+		<Container $backgroundColor={"#1b1b1d"} $padding={"15px"} $gap={"15px"}>
+			<ContainerFlexSameFlex $flex={"2"} $padding={"0px"}>
+				<FlatList
+					data={images}
+					horizontal
+					pagingEnabled
+					showsHorizontalScrollIndicator={false}
+					keyExtractor={(item) => item.id}
+					renderItem={renderItem}
+					onViewableItemsChanged={onViewableItemsChanged}
+					viewabilityConfig={{ itemVisiblePercentThreshold: 50 }}
+				/>
+				<View
+					style={{
+						flexDirection: "row",
+						justifyContent: "center",
+						alignItems: "center",
+						marginTop: 10,
+					}}
+				></View>
+			</ContainerFlexSameFlex>
+			<ContainerFlexSameFlex $backgroundColor={"#bd54eb"} $borderRadius={"20px"} $column>
+				<TextCustom $fontSize={"22px"} $fontWeight={"bold"}>
+					{detailRoutine.data.routineName}
+				</TextCustom>
+				<ContainerFlexSameFlex $justifyContent={"center"} $alignItems={"center"}>
+					<ScrollView>
+						<TextCustom $textAlign={"left"} $fontSize={"18px"}>
+							{detailRoutine.data.routineDescription}
+						</TextCustom>
+					</ScrollView>
 				</ContainerFlexSameFlex>
-				<ContainerFlexSameFlex $backgroundColor={"#bd54eb"} $borderRadius={"20px"} $column>
-					<TextCustom $fontSize={"18px"} $fontWeight={"bold"}>
-						Exercise 1
-					</TextCustom>
-					<ContainerFlexSameFlex>
-						<ScrollView>
-							<TextCustom $textAlign={"left"}>
-								Adipisicing velit aute esse ex aute ut deserunt id nostrud. Nisi cupidatat mollit aliqua occaecat duis cillum et fugiat. Cupidatat adipisicing id mollit veniam non.
-								Adipisicing occaecat labore incididunt do mollit qui dolor irure enim eu incididunt reprehenderit minim. In aute ipsum commodo aliqua sunt duis magna in.
-								Adipisicing velit aute esse ex aute ut deserunt id nostrud. Nisi cupidatat mollit aliqua occaecat duis cillum et fugiat. Cupidatat adipisicing id mollit veniam non.
-								Adipisicing occaecat labore incididunt do mollit qui dolor irure enim eu incididunt reprehenderit minim. In aute ipsum commodo aliqua sunt duis magna in.
-							</TextCustom>
-						</ScrollView>
-					</ContainerFlexSameFlex>
-				</ContainerFlexSameFlex>
-			</Container>
-		</SafeAreaView>
+			</ContainerFlexSameFlex>
+		</Container>
+
 	);
 };
 
